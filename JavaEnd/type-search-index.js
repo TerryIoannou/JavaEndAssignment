5034c37cce83271d1d2b5f0c6c016d685714cfb7
TerryIoannou <91316685+TerryIoannou@users.noreply.github.com>
2023-11-07 22:17:12 +0100

    Delete JavaEnd directory
    Needed to fix something

--- JavaEnd/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"DeliveryApplication"},{"p":"<Unnamed>","l":"Depot"},{"p":"<Unnamed>","l":"Package"},{"p":"<Unnamed>","l":"PackageType"},{"p":"<Unnamed>","l":"Person"},{"p":"<Unnamed>","l":"Postman"}];updateSearchResults();
